b8264082c7411f9f4af7876eea137e8a075da251
Ximena Castañeda Ochoa <xcastaneda@unal.edu.co>
2021-04-16 18:15:27 -0500

    Atributos habitacion

--- src/gestorAplicacion/Funcionamiento/Habitacion.java
+++ src/gestorAplicacion/Funcionamiento/Habitacion.java
@@ -1,5 +1,13 @@
 package gestorAplicacion.Funcionamiento;
+import gestorAplicacion.Cliente;
 
 public class Habitacion {
+	public int numHabitacion;
+	public boolean disponibilidad;
+	public static int capacidad2;
+	public static int capacidad3;
+	public static int capacidad4;
+	public static int capacidad5;
+	public Cliente cliente;
 
 }
