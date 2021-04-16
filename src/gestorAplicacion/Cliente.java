cc4529562b618bf36d457f208be449063c367eb3
YojanAlcaraz28 <79714839+YojanAlcaraz28@users.noreply.github.com>
2021-04-16 17:57:02 -0500

    Update Cliente.java

--- src/gestorAplicacion/Cliente.java
+++ src/gestorAplicacion/Cliente.java
@@ -1,5 +1,18 @@
 package gestorAplicacion;
 
+import gestorAplicacion.Funcionamiento.*;
+
 public class Cliente {
+	private String nombre;
+	private long cedulaIdentidad;
+	private int numNoches;
+	private String parentescoEmpleado;
+	private int numAcompañantes;
+	private boolean reserva;
+	private int cuentaFinal;
+	private int saldo;
+	private Habitacion habitacion;
+	private Hotel hotel;
+	
 
 }
