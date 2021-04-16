1d82d1e45ff816b9cb280e111bf23a026de0d1cf
Ximena Castañeda Ochoa <xcastaneda@unal.edu.co>
2021-04-16 18:46:34 -0500

    Clase servicio

--- src/gestorAplicacion/Funcionamiento/Servicio.java
+++ src/gestorAplicacion/Funcionamiento/Servicio.java
@@ -1,5 +1,34 @@
 package gestorAplicacion.Funcionamiento;
+import java.util.ArrayList;
+import gestorAplicacion.Cliente;
 
 public class Servicio {
+	public ArrayList<Integer> gastosServicios;
+	public 	Cliente cliente;
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cliente getCliente() {
+		return this.cliente;
+	}
+
+	public int reportarGasto() {
+		int reporteGasto = 0;
+		return reporteGasto;
+	}
+	
+	public void tipoMenu() {
+		
+	}
+	
+	public void tipoAtraccion() {
+		
+	}
+
+
+
+
 
 }
