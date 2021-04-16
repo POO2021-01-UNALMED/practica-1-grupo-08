37d874d62389cadf882dd62816ca6d84428c1f6e
YojanAlcaraz28 <79714839+YojanAlcaraz28@users.noreply.github.com>
2021-04-16 18:45:37 -0500

    Constructores, setters y gettters
    clase Cliente

--- src/gestorAplicacion/Cliente.java
+++ src/gestorAplicacion/Cliente.java
@@ -2,6 +2,8 @@ package gestorAplicacion;
 
 import gestorAplicacion.Funcionamiento.*;
 
+import gestorAplicacion.Personal.*;
+
 public class Cliente {
 	private String nombre;
 	private long cedulaIdentidad;
@@ -9,10 +11,80 @@ public class Cliente {
 	private String parentescoEmpleado;
 	private int numAcompañantes;
 	private boolean reserva;
-	private int cuentaFinal;
-	private int saldo;
-	private Habitacion habitacion;
-	private Hotel hotel;
+	public int cuentaFinal;
+	public int saldo;
+	public Habitacion habitacion;
+	public Hotel hotel;
 	
+	
+	public Cliente(String nombre,long cedula,int numNoches,int saldo) {
+		this.nombre = nombre;
+		this.cedulaIdentidad = cedula;
+		this.numNoches = numNoches;
+		this.saldo = saldo;
+	}
+	
+	public Cliente() {
+		
+	}
+	
+	//Setter y getters
 
+	public int getNumNoches() {
+		return numNoches;
+	}
+
+	public void setNumNoches(int numNoches) {
+		this.numNoches = numNoches;
+	}
+
+	public String getParentescoEmpleado() {
+		return parentescoEmpleado;
+	}
+
+	public void setParentescoEmpleado(String parentescoEmpleado) {
+		this.parentescoEmpleado = parentescoEmpleado;
+	}
+
+	public int getNumAcompañantes() {
+		return numAcompañantes;
+	}
+
+	public void setNumAcompañantes(int numAcompañantes) {
+		this.numAcompañantes = numAcompañantes;
+	}
+
+	public boolean isReserva() {
+		return reserva;
+	}
+
+	public void setReserva(boolean reserva) {
+		this.reserva = reserva;
+	}
+
+	public int getCuentaFinal() {
+		return cuentaFinal;
+	}
+
+	public void setCuentaFinal(int cuentaFinal) {
+		this.cuentaFinal = cuentaFinal;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+
+	public Habitacion getHabitacion() {
+		return habitacion;
+	}
+
+	public void setHabitacion(Habitacion habitacion) {
+		this.habitacion = habitacion;
+	}
+	
+	
 }
