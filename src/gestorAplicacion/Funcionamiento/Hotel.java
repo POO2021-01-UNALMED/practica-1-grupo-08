9d4099c2349cc95ae404e2013f36f7201ca394f0
vseguro <79714681+vseguro@users.noreply.github.com>
2021-04-17 17:44:40 -0500

    AsignarH

--- src/gestorAplicacion/Funcionamiento/Hotel.java
+++ src/gestorAplicacion/Funcionamiento/Hotel.java
@@ -5,13 +5,37 @@ public class Hotel {
 	public Cliente cliente;
 	public Habitacion habitacion;
 	
+	// habitaciones con capacidad dos[[103,104,105,106,107],[103,104,105,106,107]]
 	public void asignarHabitacion(Cliente cliente) {
+		int cap2 =Habitacion.getCapacidad2();
+		int cap3 =Habitacion.getCapacidad3();
+		int cap4 =Habitacion.getCapacidad4();
+		int cap5 =Habitacion.getCapacidad5();
 		if (Habitacion.disponibilidad(cliente.getNumAcompañantes())==true) {//va a disponibilidad con el parametro de num de clientes
-			if((cliente.getNumAcompañantes()+1==1)||(cliente.getNumAcompañantes()+1==2)) {
-				Habitacion.setCapacidad2(Habitacion.getCapacidad2());
+			if((cliente.getNumAcompañantes()+1==1)||(cliente.getNumAcompañantes()+1==2)) {				
+				Habitacion.setCapacidad2(cap2-1);
 				
-			}
-		} 
+			}else if ((cliente.getNumAcompañantes()+1==3)) {
+				Habitacion.setCapacidad3(cap3-1);
+				
+			}else if ((cliente.getNumAcompañantes()+1==4)) {
+				Habitacion.setCapacidad4(cap4-1);
+				
+		    }else if ((cliente.getNumAcompañantes()+1==5)) {
+				Habitacion.setCapacidad5(cap5-1);
+			}  
+		for(int i=0; i< Habitacion.habitaciones.size();i++) {
+			if(Habitacion.habitaciones.get(i).getTipoCapacidad()==cliente.getNumAcompañantes()+1) {
+				cliente.setHabitacion(Habitacion.habitaciones.get(i));
+				Habitacion.habitaciones.get(i).setCliente(cliente);	
+			}			
+		}
+		return;	
+		}
 		
+		//¿desea hacer reserva?
+		
+	
 	}
+
 }
