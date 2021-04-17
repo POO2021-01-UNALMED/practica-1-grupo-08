b65b35f108591d35a8d209b7a8ec2b3be54aeb2d
vseguro <79714681+vseguro@users.noreply.github.com>
2021-04-16 22:19:50 -0500

    Empleado

--- src/gestorAplicacion/Personal/Administrador.java
+++ src/gestorAplicacion/Personal/Administrador.java
@@ -1,7 +1,19 @@
 package gestorAplicacion.Personal;
 
 public class Administrador extends Empleado{
-	public Administrador() {}
+	public Administrador(String nombre,long id, String cargo, HorasExtras horasextras) {
+		super(nombre,id,cargo,horasextras);
+		super.asignarSalario();
+		super.pagoHorasExtras();
+	}
+	public Administrador(String nombre,long id, String cargo) {
+		super(nombre,id,cargo);
+	}
 	
 	public void pagarSalario() {}
+	
+	@Override
+	public void asignarSalario() {
+		super.setSalario(3500000);
+	}
 }
