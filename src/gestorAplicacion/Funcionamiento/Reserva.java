c6ea471aaef120142d2bee7360f11c6c133c90cd
YojanAlcaraz28 <79714839+YojanAlcaraz28@users.noreply.github.com>
2021-04-16 19:14:03 -0500

    Update Reserva.java

--- src/gestorAplicacion/Funcionamiento/Reserva.java
+++ src/gestorAplicacion/Funcionamiento/Reserva.java
@@ -1,5 +1,13 @@
 package gestorAplicacion.Funcionamiento;
 
+import gestorAplicacion.*;
+
+import java.util.Date;
+
 public class Reserva {
+	private Date fecha_de_ingreso;
+	private Date fecha_de_salida;
+	public boolean estado;
+	public Cliente cliente;
 
 }
