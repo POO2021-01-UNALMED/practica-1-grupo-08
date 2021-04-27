35580f7d48bf2c110a38db5eddc30041f7c1d4c7
Ximena Castañeda Ochoa <xcastaneda@unal.edu.co>
2021-04-27 14:39:13 -0500

    Recepción

--- src/uiMain/Recepcion.java
+++ src/uiMain/Recepcion.java
@@ -1,12 +1,42 @@
 package uiMain;
-
+import java.util.Scanner;
 import gestorAplicacion.Funcionamiento.Hotel;
 import gestorAplicacion.Personal.Administrador;
 import gestorAplicacion.Personal.Mucama;
 
 public class Recepcion {
-	public static Hotel hotel = new Hotel();
 	public static Administrador ad1 = new Administrador("Luis",134344);
+	public static Hotel hotel = new Hotel();
+	
+	static Scanner sc = new Scanner(System.in);
+	static long readLong() {
+		return sc.nextLong();
+	}
+	
+	static String readIn() {
+		sc.nextLine();
+		return sc.nextLine();
+		
+	}
+	
+	public static void main(String[] args) {
+		 
+		 
+			
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
