6558e8b70cb17dd0f4b817c5b85faac53dde7874
vseguro <79714681+vseguro@users.noreply.github.com>
2021-08-14 14:54:20 -0500

    Create GUI.java

---
+++ src/uiMain/GUI.java
@@ -0,0 +1,19 @@
+package uiMain;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+	
+	 public void start(Stage ventana) throws Exception{
+		   
+	        ventana.setTitle("Hotel");
+	        ventana.show();
+	    }
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
