390f07113f0326c8844b1c36bb5b8fa32adfc23c
vseguro <79714681+vseguro@users.noreply.github.com>
2021-08-22 12:25:01 -0500

    Ventana de inicio

--- src/uiMain/GUI.java
+++ src/uiMain/GUI.java
@@ -7,10 +7,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
@@ -24,38 +30,68 @@ public class GUI extends Application {
 		   	VBox p1 = new VBox();
 		   	Label saludo = new Label("Bienvenidos al hotel, deseamos disfrute su estadía.\n Cualquier inquietud no dude en contactarse con nosotros.");
 		   	saludo.setWrapText(true);
-		   	saludo.setPrefSize(300, 200);
+		   	saludo.setPrefSize(400, 200);
 		   	Font tipoletra = new Font("Times New Roman", 25);
 		   	saludo.setFont(tipoletra);
+		   	saludo.setTextFill(Color.web("#873600"));
 		   	saludo.setTextAlignment(TextAlignment.CENTER);
-		   	p1.setPadding(new Insets(10,10,10,30));
+		   	saludo.setStyle("-fx-background-color: #FCF3CF ;");
+		   	p1.setPadding(new Insets(10,15,10,15));
 		   	p1.setSpacing(10);
-		   	saludo.setStyle("-fx-background-color: #ECD1CC ;");
-		   	
-				   	
+		   	p1.setPrefSize(400, 550);
+		   		   	
 		   	//Panel para imagenes hotal y boton para ventana principal
 		   	BorderPane pimagenes = new BorderPane();
 		   	Button botonprincipal = new Button("Menú principal.");
 		   	pimagenes.setPrefSize(300, 300);
 		   	//Etiqueta para agregar imagenes
-		   	Image imagen1 = new Image(getClass().getResourceAsStream("./Imagenes/images.jpg"),300,250,false,false);
-		   	Label label1 = new Label("", new ImageView(imagen1));
-		   	pimagenes.setStyle("-fx-background-color: #ECD1CC ;");
-		   	BorderPane.setAlignment(label1, Pos.TOP_CENTER);
-		   	pimagenes.setTop(label1);
-		   	
-		   //	label1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-		   	   	
-		   			   		   	
+		   	Image imagen = new Image(getClass().getResourceAsStream("./Imagenes/images.jpg"),350,250,false,false);
+		   	Label label = new Label("", new ImageView(imagen));
+		   	BorderPane.setAlignment(label, Pos.TOP_CENTER);
+		   	pimagenes.setTop(label);
+		   	p1.setStyle("-fx-background-color: #FCF3CF ;");
+		   	pimagenes.setStyle("-fx-background-color:#FCF3CF ;");	   		   	
 		   	pimagenes.setBottom(botonprincipal);
-		   	//pimagenes.setCenter(label1);
 		   	p1.getChildren().addAll(saludo,pimagenes);
-		   	
 		   			   	
 		   	//Panel vertical para almacenar hoja de vida y fotos.
 		   	VBox p2 = new VBox();
+		   	p2.setSpacing(5);
+		   	p2.setPadding(new Insets(10,0,0,0));
 		   	
+		   	Label hojaVida = new Label();
+		   	hojaVida.setText("Hojas de vida de los colaboradores.");
+		   	hojaVida.setPrefSize(380, 200);
+		   	hojaVida.setWrapText(true);
+		   	hojaVida.setFont(new Font("Times New Roman", 25));		   	
+		   	hojaVida.setStyle("-fx-font-alignment: center");
+		   	hojaVida.setPadding(new Insets(0,0,0,10));
+		   	hojaVida.setStyle("-fx-border-color: white;"); //Color del borde del label  	
+		   	hojaVida.setTextFill(Color.web("#873600"));
+		   	p2.getChildren().add(hojaVida);
+		    p2.setStyle("-fx-background-color: #FCF3CF ;");
+		   	p2.setPrefSize(400,550);
 		   	
+		   	//Grid para las imágenes
+		   	GridPane fotos = new GridPane();
+		   	fotos.setPrefSize(380, 200);
+		   	fotos.setStyle("-fx-background-color:#FCF3CF ;");
+		   	fotos.setVgap(5);
+		   	fotos.setHgap(5);
+		   	Image imagen1 = new Image(getClass().getResourceAsStream("./Imagenes/imagen1.jpg"), 187.5, 150, false, false);
+		   	Label label1 = new Label("", new ImageView(imagen1)); 	
+		   	Image imagen2 = new Image(getClass().getResourceAsStream("./Imagenes/imagen2.jpg"), 187.5, 150, false, false);
+		   	Label label2= new Label("", new ImageView(imagen2));   	
+		   	Image imagen3= new Image(getClass().getResourceAsStream("./Imagenes/imagen3.jpg"), 187.5, 150, false, false);
+		   	Label label3 = new Label("", new ImageView(imagen3));	
+		   	Image imagen4 = new Image(getClass().getResourceAsStream("./Imagenes/imagen4.jpg"), 187.5, 150, false, false);
+		   	Label label4 = new Label("", new ImageView(imagen4));
+		   	fotos.add(label1, 0, 0);
+		   	fotos.add(label2, 1, 0);
+		   	fotos.add(label3, 0, 1);
+		   	fotos.add(label4, 1, 1);  	
+		   	p2.getChildren().add(fotos);
+		   
 		   	//Panel principal.
 		   	BorderPane principal = new BorderPane();
 		   	principal.setLeft(p1);
@@ -64,13 +100,11 @@ public class GUI extends Application {
 		   	Menu inicio = new Menu("Inicio");
 		   	barramenu.getMenus().add(inicio);
 		   	principal.setTop(barramenu);
-		   	
-		   	
-		   	
-		   	
+		   	principal.setStyle("-fx-background-color:#FCF3CF ;");
+		   		
 		   	//Escena 1: Ventana de incio.
-		   	Scene escena1 = new Scene(principal,800,600);
-		   	
+		   	Scene escena1 = new Scene(principal,800,550);
+		   	ventana.setResizable(false);
 		   	ventana.setScene(escena1);
 	        ventana.show();
 	    }
