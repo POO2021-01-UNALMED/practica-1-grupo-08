15e31f0193c19f6e9f273e55652ae53ef35791f5
My Name <Xcastaneda@unal.edu.co>
2021-08-27 18:09:48 -0500

    Funcionalidad 1

--- src/uiMain/Funcionalidades.java
+++ src/uiMain/Funcionalidades.java
@@ -2,6 +2,8 @@ package uiMain;
 
 import java.util.ArrayList;
 
+import baseDatos.Deserializacion;
+import gestorAplicacion.Funcionamiento.Habitacion;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -12,14 +14,16 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Funcionalidades extends Application {
+	public static VBox principal;
+	public static MenuBar barramenu;
+	public static Stage ventanaF;
+	
 	
 	 public void start(Stage ventanaF) throws Exception{
-		   	ventanaF.setTitle("Hotel");
-		   	
-		   	VBox principal = new VBox(); // Contendrá todas zonas de la ventana de funcionalidades.
-		   	Label nombre = new Label("Servicios del hotel");
-		   	
-		   	MenuBar barramenu = new MenuBar();
+		 	Funcionalidades.ventanaF = ventanaF;
+		 	ventanaF.setTitle("Servicios del hotel.");
+		    principal = new VBox(); // Contendrá todas zonas de la ventana de funcionalidades.
+		   	barramenu = new MenuBar();
 		   	
 		   	Menu archivo = new Menu("Archivo");
 		   	MenuItem aplicacion = new MenuItem("Aplicación");
@@ -42,28 +46,22 @@ public class Funcionalidades extends Application {
 			
 			barramenu.getMenus().addAll(archivo,procesos,ayuda);
 			
-			principal.getChildren().addAll(nombre,barramenu);
+			principal.getChildren().addAll(barramenu);
 			
-			//Formulario Reserva
-			/*VBox formulario = new VBox();
-			Label titulo = new Label("Titulo ensayo");
-			Label descripcion = new Label("Ensayo descripcion");
-			
-			String[] criterios = {"Cedula","Nombre","Fecha de entrada","Fecha de salida","Número de acompañantes"};
-			String[] valores = {"3254654","Luisa",null,null,null};
-			Boolean[] habilitados = {false,false,true,true,true};
-			FieldPane campos = new FieldPane("Criterio",criterios,"Valor",valores, habilitados );
-			
-			formulario.getChildren().addAll(titulo,descripcion,campos.getFieldPane());
-			principal.getChildren().add(formulario);*/
-			
+						
 			Label titulo = new Label("Bienvenido al hotel.");
 			Label descripcion = new Label("En la barra superior encontrarás los servicios que tenemos disponibles,esperamos que sean de tu agrado.");
 			Image imagen = new Image(getClass().getResourceAsStream("./Imagenes/images.jpg"),350,250,false,false);
 		   	Label label = new Label("", new ImageView(imagen));
 		   	principal.getChildren().addAll(titulo,descripcion,label);
 		   	principal.setAlignment(Pos.TOP_CENTER);
+		   			   	
 			
+		   	TomarHabitacion ensayo = new TomarHabitacion();
+		   	principal.getChildren().add(ensayo.getTomarHabitacion());
+		   	
+		   
+		   	
 			Scene estandar = new Scene(principal,800,550);
 			ventanaF.setResizable(false);
 			ventanaF.setScene(estandar);
@@ -72,6 +70,8 @@ public class Funcionalidades extends Application {
 	 }
 
 	public static void main(String[] args) {
+		Deserializacion.deserializar();
+		Habitacion.aumentarCapacidad();
 		launch(args);
 	}
 	
--- src/uiMain/TomarHabitacion.java
+++ src/uiMain/TomarHabitacion.java
@@ -1,54 +1,126 @@
 package uiMain;
 
+import java.text.Normalizer;
+import java.util.Optional;
 import gestorAplicacion.Cliente;
 import gestorAplicacion.Funcionamiento.Hotel;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.*;
 import javafx.scene.layout.*;
 
-public class TomarHabitacion{
+public class TomarHabitacion {
 	Label titulo = new Label("Tomar una habitación.");
 	Label descripcion = new Label("Para que le sea asignada una habitación por favor ingrese su número de cédula.");
 	Label criterio = new Label("Cédula:");
 	TextField campo = new TextField();
 	Button enviar = new Button("Enviar");
-	
+	GridPane panel1 = new GridPane();
 	
 	public TomarHabitacion() {
-		GridPane panel1 = new GridPane();
+		buscarCliente oyente = new buscarCliente();
+		enviar.setOnAction(oyente);
 		panel1.addRow(0, titulo);
 		panel1.addRow(1, descripcion);
-		panel1.addRow(2, criterio,campo,enviar);
+		panel1.addRow(2, criterio, campo, enviar);
 	}
-	
-	class buscarCliente implements EventHandler<ActionEvent>{
+
+	class buscarCliente implements EventHandler<ActionEvent> {
 		public void handle(ActionEvent evento) {
 			Long cedula = Long.parseLong(campo.getText());
-			boolean confirmacion = false;	
-			
+			boolean confirmacion = false;
+			Cliente clienteNuevo = null;
+
 			for (Cliente i : Hotel.getClientes()) {
 				if (cedula == i.getId()) {
+					clienteNuevo = i;
 					confirmacion = true;
 					break;
+				}
 			}
-			
-			if(confirmacion == false) {
-				campo.clear();
-				Alert sinCliente = new Alert(AlertType.WARNING);
+
+			if (confirmacion == false) {
+				Alert sinCliente = new Alert(AlertType.ERROR);
 				sinCliente.setTitle("Error");
 				sinCliente.setHeaderText("Cliente no encontrado.");
 				sinCliente.setContentText("Por favor ingrese una nueva cédula.");
-				sinCliente.show();
-				
-				//Cambiar mensaje botón - Hacer oyente de boton aceptar -> aquí va campo.clear()
+				Optional<ButtonType> result = sinCliente.showAndWait();
+
+				if (result.get() == ButtonType.OK) {
+					campo.clear();
+				}
+			} else if (clienteNuevo.getHabitacion() != null) {
+				Alert siHabitacion = new Alert(AlertType.INFORMATION);
+				siHabitacion.setTitle("Información");
+				siHabitacion.setHeaderText("Usted ya tiene una habitación asignada.");
+				siHabitacion.setContentText("Ya puedes acceder a nuestros servicios,dirígete a la barra superior.");
+				Optional<ButtonType> resulta = siHabitacion.showAndWait();
+				if (resulta.get() == ButtonType.OK) {
+					campo.clear();
+				}
+
+			} else if (confirmacion == true) {
+				Hotel.asignarHabitacion(clienteNuevo);
+				if (clienteNuevo.getHabitacion() != null) {
+					Alert siHabitacion = new Alert(AlertType.INFORMATION);
+					siHabitacion.setTitle("Información");
+					siHabitacion.setHeaderText("Habitación asignada con éxito.");
+					siHabitacion.setContentText("La habitación que le ha sido asignada es: "
+							+ clienteNuevo.getHabitacion().getNumhabitacion());
+					Optional<ButtonType> resultad = siHabitacion.showAndWait();
+					if (resultad.get() == ButtonType.OK) {
+						campo.clear();
+					}
+				} else if (clienteNuevo.getHabitacion() == null) {
+					Alert noHabitacion = new Alert(AlertType.CONFIRMATION);
+					noHabitacion.setTitle("Confirmación.");
+					noHabitacion.setHeaderText("No se encuentran habitaciones disponibles.");
+					noHabitacion.setContentText("¿Desea reservar una habitación?");
+					ButtonType si = new ButtonType("Sí", ButtonBar.ButtonData.YES);
+					ButtonType no = new ButtonType("No", ButtonBar.ButtonData.NO);
+					noHabitacion.getButtonTypes().setAll(si, no);
+					
+					Optional<ButtonType> resultado = noHabitacion.showAndWait();
+					if (resultado.get().equals(noHabitacion.getButtonTypes().get(1))) {
+						campo.clear();
+						Alert adios = new Alert(AlertType.INFORMATION);
+						adios.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("./Imagenes/triste.png"),50,50,false,false)));
+						adios.setHeaderText("No se encuentran habitaciones disponibles.");
+						adios.setContentText("¡Gracias por elegirnos, esperamos tener disponibilidad la próxima ocasión!");
+						adios.show();
+					} else if (resultado.get().equals(noHabitacion.getButtonTypes().get(0))) {
+						//Formulario Reserva
+						VBox formulario = new VBox();
+						Label titulo = new Label("Titulo ensayo");
+						Label descripcion = new Label("Ensayo descripcion");
+						
+						String[] criterios = {"Cedula","Nombre","Fecha de entrada","Fecha de salida","Número de acompañantes"};
+						String[] valores = {String.valueOf(clienteNuevo.getId()),clienteNuevo.getNombre(),"yyyy/mm/dd","yyyy/mm/dd",null};
+						Boolean[] habilitados = {false,false,true,true,true};
+						FieldPane campos = new FieldPane("Criterio",criterios,"Valor",valores, habilitados );
+						
+						formulario.getChildren().addAll(Funcionalidades.barramenu,titulo,descripcion,campos.getFieldPane());
+						Scene tomarHabitacion = new Scene(formulario,800,550);
+						Funcionalidades.ventanaF.setScene(tomarHabitacion);
+						
+					}
+					/*
+					 * if (respuesta.equals("si")) { hacerReserva(clientenuevo); return; } else if
+					 * (respuesta.equals("no")) { System.out.
+					 * println("¡Gracias por elegirnos, esperamos tener disponibilidad la próxima ocasión!"
+					 * ); }
+					 */
+				}
 			}
-			
+
 		}
 	}
-	
-	
-	}	
+
+	public GridPane getTomarHabitacion() {
+		return panel1;
+	}
 
 }
